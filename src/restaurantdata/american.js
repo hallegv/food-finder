d52ea77c594fc36464e93e7e7029af0a4704a9eb
Aidan Srouji <72050533+asrouji@users.noreply.github.com>
2021-03-08 15:13:48 -0800

    Add american.js and mexican.js

---
+++ src/restaurantdata/american.js
@@ -0,0 +1,132 @@
+export const american = {
+  "businesses": [
+    {
+      "id": "RYA5VQ_8BJS6xilYG-BPkg",
+      "alias": "in-n-out-burger-los-angeles-2",
+      "name": "In-N-Out Burger",
+      "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/ETH8R4dCjDl_Tjo1KFKLaQ/o.jpg",
+      "is_closed": false,
+      "url": "https://www.yelp.com/biz/in-n-out-burger-los-angeles-2?adjust_creative=d18QTjBmI-Zd-PmqUlDvvA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=d18QTjBmI-Zd-PmqUlDvvA",
+      "review_count": 1565,
+      "categories": [
+          {
+              "alias": "burgers",
+              "title": "Burgers"
+          },
+          {
+              "alias": "hotdogs",
+              "title": "Fast Food"
+          }
+      ],
+      "rating": 4.0,
+      "coordinates": {
+          "latitude": 33.9537021005473,
+          "longitude": -118.396771789024
+      },
+      "transactions": [],
+      "price": "$",
+      "location": {
+          "address1": "9149 S Sepulveda Blvd",
+          "address2": null,
+          "address3": "",
+          "city": "Los Angeles",
+          "zip_code": "90045",
+          "country": "US",
+          "state": "CA",
+          "display_address": [
+              "9149 S Sepulveda Blvd",
+              "Los Angeles, CA 90045"
+          ]
+      },
+      "phone": "+18007861000",
+      "display_phone": "(800) 786-1000",
+      "distance": 647.6409689410108
+    },
+    {
+      "id": "M2mLuHbmDzwR8QMGqdLjrQ",
+      "alias": "truxtons-american-bistro-los-angeles",
+      "name": "Truxton's American Bistro",
+      "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/C_dt3PCk_oF28Tqa4xiBPA/o.jpg",
+      "is_closed": false,
+      "url": "https://www.yelp.com/biz/truxtons-american-bistro-los-angeles?adjust_creative=d18QTjBmI-Zd-PmqUlDvvA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=d18QTjBmI-Zd-PmqUlDvvA",
+      "review_count": 1442,
+      "categories": [
+          {
+              "alias": "newamerican",
+              "title": "American (New)"
+          }
+      ],
+      "rating": 4.0,
+      "coordinates": {
+          "latitude": 33.959442,
+          "longitude": -118.393598
+      },
+      "transactions": [
+          "pickup",
+          "delivery"
+      ],
+      "price": "$$",
+      "location": {
+          "address1": "8611 Truxton Ave",
+          "address2": null,
+          "address3": "",
+          "city": "Los Angeles",
+          "zip_code": "90045",
+          "country": "US",
+          "state": "CA",
+          "display_address": [
+              "8611 Truxton Ave",
+              "Los Angeles, CA 90045"
+          ]
+      },
+      "phone": "+13104178789",
+      "display_phone": "(310) 417-8789",
+      "distance": 1159.915722810595
+    },
+    {
+      "id": "7GNQast7U3fmiVXRJoCOlg",
+      "alias": "mendocino-farms-marina-del-rey",
+      "name": "Mendocino Farms",
+      "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/mUl7xpLm5NN13J4mErIluQ/o.jpg",
+      "is_closed": false,
+      "url": "https://www.yelp.com/biz/mendocino-farms-marina-del-rey?adjust_creative=d18QTjBmI-Zd-PmqUlDvvA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=d18QTjBmI-Zd-PmqUlDvvA",
+      "review_count": 2000,
+      "categories": [
+          {
+              "alias": "sandwiches",
+              "title": "Sandwiches"
+          },
+          {
+              "alias": "salad",
+              "title": "Salad"
+          }
+      ],
+      "rating": 4.5,
+      "coordinates": {
+          "latitude": 33.978883,
+          "longitude": -118.437869
+      },
+      "transactions": [
+          "pickup",
+          "delivery"
+      ],
+      "price": "$$",
+      "location": {
+          "address1": "4724 Admiralty Way",
+          "address2": "",
+          "address3": "",
+          "city": "Marina Del Rey",
+          "zip_code": "90292",
+          "country": "US",
+          "state": "CA",
+          "display_address": [
+              "4724 Admiralty Way",
+              "Marina Del Rey, CA 90292"
+          ]
+      },
+      "phone": "+13108222300",
+      "display_phone": "(310) 822-2300",
+      "distance": 4235.124785705414
+    }
+  ]
+};
---
+++ src/restaurantdata/mexican.js
@@ -0,0 +1,192 @@
+export const mexican = {
+  "businesses": [
+    {
+      "id": "uA3TautyZ7bby0w8Scwufg",
+      "alias": "bennys-tacos-and-chicken-rotisserie-los-angeles-4",
+      "name": "Benny's Tacos & Chicken Rotisserie",
+      "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/DvYYgTEklvMM_HMhbMX1rA/o.jpg",
+      "is_closed": false,
+      "url": "https://www.yelp.com/biz/bennys-tacos-and-chicken-rotisserie-los-angeles-4?adjust_creative=d18QTjBmI-Zd-PmqUlDvvA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=d18QTjBmI-Zd-PmqUlDvvA",
+      "review_count": 2588,
+      "categories": [
+          {
+              "alias": "mexican",
+              "title": "Mexican"
+          },
+          {
+              "alias": "tex-mex",
+              "title": "Tex-Mex"
+          }
+      ],
+      "rating": 4.5,
+      "coordinates": {
+          "latitude": 33.95998,
+          "longitude": -118.41685
+      },
+      "transactions": [
+          "delivery",
+          "pickup"
+      ],
+      "price": "$",
+      "location": {
+          "address1": "7101 W Manchester Ave",
+          "address2": "",
+          "address3": "",
+          "city": "Los Angeles",
+          "zip_code": "90045",
+          "country": "US",
+          "state": "CA",
+          "display_address": [
+              "7101 W Manchester Ave",
+              "Los Angeles, CA 90045"
+          ]
+      },
+      "phone": "+13106708226",
+      "display_phone": "(310) 670-8226",
+      "distance": 1418.695146750119
+    },
+    {
+      "id": "VscSML6lgNgjJbm41Jqjog",
+      "alias": "sol-mexican-cocina-los-angeles-3",
+      "name": "SOL Mexican Cocina",
+      "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/VzfWhm944uJ6raV3XRoUDA/o.jpg",
+      "is_closed": false,
+      "url": "https://www.yelp.com/biz/sol-mexican-cocina-los-angeles-3?adjust_creative=d18QTjBmI-Zd-PmqUlDvvA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=d18QTjBmI-Zd-PmqUlDvvA",
+      "review_count": 1338,
+      "categories": [
+          {
+              "alias": "bars",
+              "title": "Bars"
+          },
+          {
+              "alias": "mexican",
+              "title": "Mexican"
+          },
+          {
+              "alias": "breakfast_brunch",
+              "title": "Breakfast & Brunch"
+          }
+      ],
+      "rating": 4.0,
+      "coordinates": {
+          "latitude": 33.97607616706,
+          "longitude": -118.416961589194
+      },
+      "transactions": [
+          "delivery",
+          "pickup"
+      ],
+      "price": "$$",
+      "location": {
+          "address1": "12775 W Millennium Dr",
+          "address2": "Ste 160",
+          "address3": "",
+          "city": "Los Angeles",
+          "zip_code": "90094",
+          "country": "US",
+          "state": "CA",
+          "display_address": [
+              "12775 W Millennium Dr",
+              "Ste 160",
+              "Los Angeles, CA 90094"
+          ]
+      },
+      "phone": "+14242890066",
+      "display_phone": "(424) 289-0066",
+      "distance": 2807.5857906326846
+    },
+    {
+      "id": "fd7ogawotk0rXafXahdS3Q",
+      "alias": "señor-gs-fresh-and-healthy-mexican-food-playa-del-rey-4",
+      "name": "Señor G's Fresh & Healthy Mexican Food",
+      "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/2qbutcLrSW9ePu4UAuXGKg/o.jpg",
+      "is_closed": false,
+      "url": "https://www.yelp.com/biz/se%C3%B1or-gs-fresh-and-healthy-mexican-food-playa-del-rey-4?adjust_creative=d18QTjBmI-Zd-PmqUlDvvA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=d18QTjBmI-Zd-PmqUlDvvA",
+      "review_count": 662,
+      "categories": [
+          {
+              "alias": "mexican",
+              "title": "Mexican"
+          },
+          {
+              "alias": "juicebars",
+              "title": "Juice Bars & Smoothies"
+          },
+          {
+              "alias": "breakfast_brunch",
+              "title": "Breakfast & Brunch"
+          }
+      ],
+      "rating": 4.5,
+      "coordinates": {
+          "latitude": 33.960656,
+          "longitude": -118.446662
+      },
+      "transactions": [
+          "delivery"
+      ],
+      "price": "$$",
+      "location": {
+          "address1": "343 Culver Blvd",
+          "address2": "",
+          "address3": "",
+          "city": "Playa Del Rey",
+          "zip_code": "90293",
+          "country": "US",
+          "state": "CA",
+          "display_address": [
+              "343 Culver Blvd",
+              "Playa Del Rey, CA 90293"
+          ]
+      },
+      "phone": "+13108227733",
+      "display_phone": "(310) 822-7733",
+      "distance": 4040.6928737135954
+    },
+    {
+      "id": "wZO-ALpCQSVPpsMxPMK_Cg",
+      "alias": "cinco-los-angeles-2",
+      "name": "Cinco",
+      "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/lwrm8XpODrovlVuLtaY1YA/o.jpg",
+      "is_closed": false,
+      "url": "https://www.yelp.com/biz/cinco-los-angeles-2?adjust_creative=d18QTjBmI-Zd-PmqUlDvvA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=d18QTjBmI-Zd-PmqUlDvvA",
+      "review_count": 597,
+      "categories": [
+          {
+              "alias": "mexican",
+              "title": "Mexican"
+          },
+          {
+              "alias": "bars",
+              "title": "Bars"
+          }
+      ],
+      "rating": 4.0,
+      "coordinates": {
+          "latitude": 33.960296,
+          "longitude": -118.42056
+      },
+      "transactions": [
+          "pickup",
+          "delivery"
+      ],
+      "price": "$$",
+      "location": {
+          "address1": "7241 W Manchester Ave",
+          "address2": "",
+          "address3": "",
+          "city": "Los Angeles",
+          "zip_code": "90045",
+          "country": "US",
+          "state": "CA",
+          "display_address": [
+              "7241 W Manchester Ave",
+              "Los Angeles, CA 90045"
+          ]
+      },
+      "phone": "+13109100895",
+      "display_phone": "(310) 910-0895",
+      "distance": 1731.4539753986971
+    }
+  ]
+}
